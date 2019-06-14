e5ad43e07015f7a7645318e5b66795e159ad8a60
1127071993@qq.com <aini2009626>
2019-06-14 21:46:47 +0800

    p16

--- src/filter/moneyFilter.js
+++ src/filter/moneyFilter.js
@@ -1,4 +1,4 @@
-export function toMoney(money){
+/*export function toMoney(money){
   if(money){
     money = money.toFixed(2);
   }else{
@@ -6,4 +6,9 @@ export function toMoney(money){
     money = money.toFixed(2);
   }
 return money;
+}*/
+
+/*<!-- 代码优化 -->*/
+export function toMoney(money=0){
+  return money.toFixed(2)
 }
